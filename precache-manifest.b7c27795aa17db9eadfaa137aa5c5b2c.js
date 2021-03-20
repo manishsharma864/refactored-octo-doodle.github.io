9f8f37406ffd49c4cfd28ce85ba079ef45d3b60f
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2021-03-20 14:23:53 +0530

    Add files via upload

---
+++ precache-manifest.b7c27795aa17db9eadfaa137aa5c5b2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9fe32fde1321915facb",
+    "url": "/static/css/main.696b90f0.chunk.css"
+  },
+  {
+    "revision": "f9fe32fde1321915facb",
+    "url": "/static/js/main.f9fe32fd.chunk.js"
+  },
+  {
+    "revision": "e71fa84e2ccdbf725035",
+    "url": "/static/js/1.e71fa84e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ec3c114e2b646b7b8b16ec554e56380c",
+    "url": "/index.html"
+  }
+];
